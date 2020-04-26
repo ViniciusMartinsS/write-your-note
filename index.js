d74f7f7c68f643cbe7e7d359f6d29c269d6e09bb
Vinicius Martins <visimonemartins@hotmail.com>
2020-04-26 19:05:39 -0300

    master update way of refirect file

--- index.js
+++ index.js
@@ -1,8 +1,6 @@
 'use strict'
 
 function createWorkspace() {
-  const REGEX = /(index.html)/i
-
   const workspace = document.getElementById("workspace")
     .value
 
@@ -11,7 +9,6 @@ function createWorkspace() {
     return
   }
 
-  const newRoute = `notes/index.html?workspace=${workspace}`
-  const redirect = document.URL.replace(REGEX, newRoute)
+  const redirect = `${document.URL}/notes/index.html?workspace=${workspace}`
   window.location.href = redirect;
 }
